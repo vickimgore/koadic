21d42d042965447d972ab4cfa6b6de698348decc
Jenna Magius <pyrosim@gmail.com>
2018-04-24 22:27:42 -0600

    Don't lose the prompt

--- core/shell.py
+++ core/shell.py
@@ -50,7 +50,7 @@ class Shell(object):
                 if len(cmd) == 0:
                     cmd = self.get_command(self.prompt, self.autocomplete)
                 else:
-                    print_plain(cmd)
+                    print_plain(self.clean_prompt + cmd)
 
                 self.run_command(cmd)
 
